36ccff5e87678918f7e0fc492c9a4a664f82a526
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2020-08-15 14:22:40 -0700

    Move these bits of config to the Dockerfile
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# Moved to Dockerfile
 #set kernel modules
-grep -q "^snd-soc-seeed-voicecard$" /etc/modules || \
-  echo "snd-soc-seeed-voicecard" >> /etc/modules
-grep -q "^snd-soc-ac108$" /etc/modules || \
-  echo "snd-soc-ac108" >> /etc/modules
-grep -q "^snd-soc-wm8960$" /etc/modules || \
-  echo "snd-soc-wm8960" >> /etc/modules
+# grep -q "^snd-soc-seeed-voicecard$" /etc/modules || \
+#   echo "snd-soc-seeed-voicecard" >> /etc/modules
+# grep -q "^snd-soc-ac108$" /etc/modules || \
+#   echo "snd-soc-ac108" >> /etc/modules
+# grep -q "^snd-soc-wm8960$" /etc/modules || \
+
 
 OS_VERSION=$(echo "$BALENA_HOST_OS_VERSION" | cut -d " " -f 2)
 echo "OS Version is $OS_VERSION"
@@ -22,10 +23,10 @@ insmod $mod_dir/snd-soc-seeed-voicecard.ko
 insmod $mod_dir/snd-soc-wm8960.ko
 lsmod | grep snd
 
-ln -s /etc/voicecard/asound_2mic.conf /etc/asound.conf
-# ln -s /etc/voicecard/ac108_asound.state /var/lib/alsa/asound.state
-# ---> WRONG: wm8960_asound.state, not ac108!
-ln -s /etc/voicecard/wm8960_asound.state /var/lib/alsa/asound.state
+# Moved to Dockerfile
+# ln -s /etc/voicecard/asound_2mic.conf /etc/asound.conf
+# ln -s /etc/voicecard/wm8960_asound.state /var/lib/alsa/asound.state
+
 alsactl restore
 amixer cset numid=3 1
 
